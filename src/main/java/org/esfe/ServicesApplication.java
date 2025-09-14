f63784f3060fe8ed6cfcb8ac51a00eacab5d2c56
Brayan Eduardo Tepas Linares <tl25001@esfe.agape.edu.sv>
2025-09-14 09:52:02 -0600

    se modifico la estructura de las carpetas para tener una carpeta padre que funciona para eureka

--- src/main/java/org/esfe/ServicesApplication.java
+++ src/main/java/org/esfe/ServicesApplication.java
@@ -1,13 +1,13 @@
-package com.ApiGateway.ApiGateway;
+package org.esfe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiGatewayApplication {
+public class ServicesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApiGatewayApplication.class, args);
+		SpringApplication.run(ServicesApplication.class, args);
 	}
 
 }
--- src/test/java/org/esfe/ServicesApplicationTests.java
+++ src/test/java/org/esfe/ServicesApplicationTests.java
@@ -1,10 +1,10 @@
-package com.ApiGateway.ApiGateway;
+package org.esfe;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApiGatewayApplicationTests {
+class ServicesApplicationTests {
 
 	@Test
 	void contextLoads() {
